0e190d478b91bdaecf435545139d603aaf2aa81f
alyona-nad <116522266+alyona-nad@users.noreply.github.com>
2023-09-26 02:01:55 +0300

    Update server.go

--- internal/api/server.go
+++ internal/api/server.go
@@ -5,17 +5,40 @@ import (
 	"net/http"
 	"strings"
 
+	"awesomeProject/internal/app/dsn"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/kljensen/snowball/russian"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-
 type ServiceProduct struct {
+	ID          int64
 	Name        string
 	Image       string
 	Link        string
 	Description string
 	Properties  string
+	Status      string
+}
+
+func GetProductsFromDB(db *gorm.DB) ([]ServiceProduct, error) {
+	var products []ServiceProduct
+	var products1 []int64
+	err := db.Table("colorants").Select("id_colorant, name, image, description, properties,status").Where("status = ?", "Действует").Scan(&products).Error
+	if err != nil {
+		return nil, err
+	}
+	err1 := db.Table("colorants").Select("id_colorant").Where("status = ?", "Действует").Scan(&products1).Error
+	for i, id := range products1 {
+		products[i].ID = id
+	}
+	if err1 != nil {
+		return nil, err1
+	}
+	return products, nil
 }
 
 func StartServer() {
@@ -25,80 +48,54 @@ func StartServer() {
 
 	r.LoadHTMLGlob("templates/*")
 
-	data := []ServiceProduct{
-		{
-			Name:        "Диоксид титана",
-			Image:       "https://static.tildacdn.com/stor3836-6631-4636-b938-336534356332/69807646.jpg",
-			Description: "Чистый диоксид титана — бесцветные кристаллы (желтеет при нагревании). Для технических целей применяется в раздробленном состоянии, представляя собой белый порошок. Не растворяется в воде и разбавленных минеральных кислотах (за исключением плавиковой).",
-			Properties:  "Плотность: 4,235 г/см3\n/nТемпература плавления: 1843°C Температура кипения: 2972°C<br> Растворимость в воде: не растворяется<br>Температура разложения: 2900°C",
-			Link:        "/product/TiO2",
-		},
-		{
-			Name:        "Оксид(III) железа",
-			Image:       "https://sc04.alicdn.com/kf/Hacb20d18e6274e53a197e9f380825000K.jpg",
-			Description: "Амфотерный оксид с большим преобладанием основных свойств. Красно-коричневого цвета. Термически устойчив до высоких температур. Образуется при сгорании железа на воздухе. Не реагирует с водой. Медленно реагирует с кислотами и щелочами.",
-			Properties:  "Плотность:	5,242 г/см3 Температура плавления:	1566°C Растворимость в воде:	нерастворим",
-			Link:        "/product/FeO2",
-		},
-		{
-			Name:        "Оксид(III) хрома",
-			Image:       "https://ddek.ru/assets/images/products/225/prasino-tsimentou.jpg",
-			Description: "Амфотерный оксид с большим преобладанием основных свойств. Красно-коричневого цвета. Термически устойчив до высоких температур. Образуется при сгорании железа на воздухе. Не реагирует с водой. Медленно реагирует с кислотами и щелочами.",
-			Properties:  "Плотность:	5,242 г/см3 Температура плавления:	1566°C Растворимость в воде:	нерастворим",
-			Link:        "/product/Cr2O3",
-		},
-		{
-			Name:        "Оксид(V) ванадия",
-			Image:       "https://chem.ru/uploads/posts/2020-04/medium/1586977299_oksid-vanadija-v.jpg",
-			Description: "Амфотерный оксид с большим преобладанием основных свойств. Красно-коричневого цвета. Термически устойчив до высоких температур. Образуется при сгорании железа на воздухе. Не реагирует с водой. Медленно реагирует с кислотами и щелочами.",
-			Properties:  "Плотность:	5,242 г/см3 Температура плавления:	1566°C Растворимость в воде:	нерастворим",
-			Link:        "/product/V2O5",
-		},
-		{
-			Name:        "Оксид(IV) марганца",
-			Image:       "https://img.promportal.su/foto/good_fotos/49789/497896103/oksid-marganca-iv-dioksid-marganca-mno2_foto_largest.jpg",
-			Description: "Амфотерный оксид с большим преобладанием основных свойств. Красно-коричневого цвета. Термически устойчив до высоких температур. Образуется при сгорании железа на воздухе. Не реагирует с водой. Медленно реагирует с кислотами и щелочами.",
-			Properties:  "Плотность:	5,242 г/см3 Температура плавления:	1566°C Растворимость в воде:	нерастворим",
-			Link:        "/product/MnO2",
-		},
-	}
-
 	r.Static("/styles", "./internal/css")
 	r.Static("/image", "./resources")
+	_ = godotenv.Load()
+
+	db, err := gorm.Open(postgres.Open(dsn.FromEnv()), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
 
 	r.GET("/home", func(c *gin.Context) {
+
+		products, err := GetProductsFromDB(db)
+		if err != nil {
+			panic("failed to get products from DB")
+		}
 
 		filterValue := c.Query("filterValue")
 
 		var filteredServices []ServiceProduct
-
 		if filterValue != "" {
 			filterValueNormalized := russian.Stem(filterValue, false)
 
-			for _, service := range data {
+			for _, service := range products {
 				serviceNameNormalized := russian.Stem(service.Name, false)
 				if strings.Contains(strings.ToLower(serviceNameNormalized), strings.ToLower(filterValueNormalized)) {
 					filteredServices = append(filteredServices, service)
 				}
 			}
 		} else {
-			filteredServices = data
+			filteredServices = products
 		}
 
-		log.Println(filteredServices)
-
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title":    "Производство красок",
-			"services": filteredServices,
-			"filterValue":filterValue,
+			"title":       "Производство красок",
+			"services":    filteredServices,
+			"filterValue": filterValue,
 		})
 	})
 
 	r.GET("/product/:name", func(c *gin.Context) {
 		productName := c.Param("name")
+		products, err := GetProductsFromDB(db)
+		if err != nil {
+			panic("failed to get products from DB")
+		}
 
 		var product ServiceProduct
-		for _, p := range data {
+		for _, p := range products {
 			if p.Name == productName {
 				product = p
 				break
@@ -111,6 +108,34 @@ func StartServer() {
 			"Image":       product.Image,
 			"Description": product.Description,
 		})
+	})
+
+	r.POST("/delete-service/:id", func(c *gin.Context) {
+		serviceID := c.Param("id")
+		var count int64
+		db.Table("colorants").Where("id_colorant = ?", serviceID).Count(&count)
+
+		if count == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Service not found"})
+			return
+		}
+
+		result := db.Exec("UPDATE colorants SET status = ? WHERE id_colorant = ?", "удалено", serviceID)
+		if result.Error != nil {
+			log.Println("Failed to delete service:", result.Error)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			return
+		}
+
+		log.Println("Service deleted successfully")
+
+		filterValue := c.Query("filterValue")
+		if filterValue != "" {
+			c.Redirect(http.StatusSeeOther, "/home?filterValue="+filterValue)
+		} else {
+			c.Redirect(http.StatusSeeOther, "/home")
+		}
+
 	})
 
 	r.Run()
